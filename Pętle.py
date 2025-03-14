0af9d6f55459a9878020986cae5c86b3a40a0f9c
Xajmaq <xajmaq@gmail.com>
2025-03-14 21:32:53 +0100

    v.0.11

--- Pętle.py
+++ Pętle.py
@@ -1,2 +1,15 @@
+# Pętle pozwalają powtarzać fragmenty kodu wiele razy
+# bez konieczności ręcznego pisania tych samych instrukcji.
+
+# Pętla for
+# Najczęściej używana do przechodzenia po elementach listy, zakresu liczb itp.
+
+# Wyświetlanie liczby od 1-10 (11 nie jest liczone!)
 for i in range(1,11):
     print(i)
+
+# Rozbija słowo na litery
+tekst = "Python"
+for litera in tekst:
+    print(litera)
+
