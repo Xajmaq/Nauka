e3489d85abb7a6fefaaa5ab006539e75cdf9fd92
Xajmaq <xajmaq@gmail.com>
2025-03-15 14:28:21 +0100

    v.0.1

---
+++ Ćwiczenia Pętle.py
@@ -0,0 +1,22 @@
+print("Ćwiczenie 1")
+
+# Wypisz liczby od 1 do 10
+
+for i in range(1, 11):
+    print(i)
+
+print ("Ćwiczenie 2")
+
+# Liczby parzyste
+
+for i in range(1, 21):
+    if i % 2 == 0:
+        print(i)
+
+print("Ćwiczenie 3")
+
+# Suma liczb od 1 do n
+
+n = int(input("Podaj liczbę n: "))
+
+
