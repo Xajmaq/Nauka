73fb23fc6cedff38fc4d11c7c260c97e29f9158b
Xajmaq <xajmaq@gmail.com>
2025-03-16 10:35:45 +0100

    v.0.11

---
+++ Operatory skróconego przypisania.py
@@ -0,0 +1,14 @@
+
+# suma += i to skórcony zapisu suma = suma + i "increment assignment operator" Działa tak:
+# Pobiera obecną wartość zmiennej.
+# Dodaje do niej wartość po prawej stronie.
+# Przypisuje nową wartość do tej samej zmiennej.
+
+#  Przykład
+suma = 0 # Początkowa wartość sumy
+suma += 5 # To samo co suma = suma + 5
+print(f"Suma liczb: {suma}") # Wynik 5
+suma += 3 # To samo co suma = suma + 3
+print(f"Suma liczb: {suma}") # Wynik 8
+
+# Inne operatory skróconego przypisania
