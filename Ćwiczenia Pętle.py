bd40d1d81afa6c46fd333e3817bc46e8ed800925
Xajmaq <xajmaq@gmail.com>
2025-03-16 10:23:31 +0100

    v.0.12

--- Ćwiczenia Pętle.py
+++ Ćwiczenia Pętle.py
@@ -18,7 +18,19 @@ print("Ćwiczenie 3")
 # Suma liczb od 1 do n
 
 n = int(input("Podaj liczbę n: "))
-for i in range(1, n + 1):
-    print(i)
+suma = 0 # Tworzymy zmienną do przechowywania sumy, początkowo suma jest równa 0 bo nic nie dodaliśmy!
 
-print(((1+n)/2)*n)
+for i in range(1, n + 1): # Zakres o 1 do n
+    suma += i # Dodajemy i do sumy
+
+# range(1, n + 1) tworzy listę liczb od 1 do n
+# suma += i oznacza suma = suma + i czyli dodajemy kolejne liczby do sumy.
+
+print("Suma liczb od 1 do", n,"wynosi:", suma)
+
+
+
+# print(((1+n)/2)*n)
+# Moje rozwiązanie :P Prosty wzór na Ciąg arytmetyczny :P Ech, stary człowiek jestem.
+
+
