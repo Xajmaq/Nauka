a0b4a0fcd1ca95cc806a0f89695fb8d395db9ca8
Xajmaq <xajmaq@gmail.com>
2025-03-16 10:49:30 +0100

    v.0.12

--- Operatory skróconego przypisania.py
+++ Operatory skróconego przypisania.py
@@ -12,3 +12,39 @@ suma += 3 # To samo co suma = suma + 3
 print(f"Suma liczb: {suma}") # Wynik 8
 
 # Inne operatory skróconego przypisania
+
+# += Dodawanie
+x = 10
+x += 5
+print(f"Suma liczb: {x}")
+
+# -= Odejmowanie
+x = 20
+x -= 7
+print(f"Rożnica liczb: {x}")
+
+# *= Mnożenie
+x = 3
+x *= 4
+print(f"Mnożenie liczb: {x}")
+
+# /= Dzielenie
+x = 10
+x /= 2
+print(f"Rożnica liczb: {x}")
+
+# //= Dzielenie całkowite
+x = 10
+x //= 3
+print(f"Rożnica liczb: {x}")
+
+# %= Zachowuje resztę z dzelenia
+x = 10
+x %= 3
+print(f"Rożnica liczb: {x}")
+
+# **= Potęgowanie
+x = 2
+x **= 3
+print(f"Rożnica liczb: {x}")
+
