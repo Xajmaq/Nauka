81f947297c2a75603d21cc6573615dedf09813fe
Xajmaq <xajmaq@gmail.com>
2025-03-16 11:27:03 +0100

    v.0.13

--- Ćwiczenia Pętle.py
+++ Ćwiczenia Pętle.py
@@ -33,4 +33,21 @@ print("Suma liczb od 1 do", n,"wynosi:", suma)
 # print(((1+n)/2)*n)
 # Moje rozwiązanie :P Prosty wzór na Ciąg arytmetyczny :P Ech, stary człowiek jestem.
 
+print("Ćwiczenie 4")
 
+# Odliczanie od 10 do 1
+# Po zakończeniu pętki wypisz "Start!"
+
+#  Pierwszy sposób z pętlą for
+
+for i in range(10, 0, -1): #10 wartość startowa, 0 wartość końcowa (0 się nie wlicza) -1 o ile się zmiejsza wartość
+    print(i)
+print("Start")
+
+# Pętla while
+
+x = 10
+while x > 0:
+    print(x)
+    x -= 1
+print("Start!")
